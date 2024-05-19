a122df34518fc259f66e4c910e07796ffabe4022
Shravaniii03 <165552755+Shravaniii03@users.noreply.github.com>
2024-05-19 10:24:49 +0530

    grocery.h

--- Project_in_c/grocery.h
+++ Project_in_c/grocery.h
@@ -13,6 +13,26 @@ void finalbill(char names[][n], float cost[], int quantities[], int Products, ch
 
 
 
+/*#ifndef GROCERY_H
+#define GROCERY_H
+
+#define length 50
+#define MAX_PRODUCTS 100
+#define GST 0.09
+
+struct Product {
+    char name[length];
+    float price;
+};
+
+void choose(struct Product products[], int numProducts);
+float total(struct Product products[], int quantities[], int numProducts);
+void finalbill(struct Product products[], int quantities[], int numProducts, char Name[], char Address[], char Phonenumber[]);
+
+#endif*/
+
+
+//updated code
 #ifndef GROCERY_H
 #define GROCERY_H
 
@@ -30,3 +50,4 @@ float total(struct Product products[], int quantities[], int numProducts);
 void finalbill(struct Product products[], int quantities[], int numProducts, char Name[], char Address[], char Phonenumber[]);
 
 #endif 
+
